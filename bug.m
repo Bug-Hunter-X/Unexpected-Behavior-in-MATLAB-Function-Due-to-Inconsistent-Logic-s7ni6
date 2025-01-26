7b8ed980fbb73e08a5fcf8003674e4755308d6a3
BugHunter-X <askzenai@gmail.com>
2025-01-26 15:46:05 +0100

    Add bug.m file

---
+++ bug.m
@@ -0,0 +1,13 @@
+function result = myFunction(x)
+  if x > 10
+    result = x * 2; 
+  elseif x < 0
+    result = 0; 
+  else
+    result = x + 5; 
+end
+
+% Example usage
+result1 = myFunction(12);  % Expected: 24
+result2 = myFunction(-5); % Expected: 0
+result3 = myFunction(5);  % Expected: 10
